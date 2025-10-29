c870ab0dcf8107ab3502558d428a04330f60101f
its_jedu <olajideweb@gmail.com>
2025-10-30 00:34:45 +0100

    Adjusted

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+  images: {
+    domains: ['payroll-backend-staging.onrender.com'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
